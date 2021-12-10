9ae7f5f30fcaa9f6335281f7828b064a8e4880b6
Bryan Engel <bryan.engel@isen.yncrea.fr>
2021-12-10 14:08:30 +0100

    Field Add ( accessor + init + start )

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -2,17 +2,44 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
-    private int width;
-    private int height;
+    private static Field instance;
+
+    private float width;
+    private float height;
     [SerializeField] private Team team1, team2;
 
-    public Vector3 BottomLeftCorner { get; private set; }
-    public Vector3 BottomRightCorner { get; private set; }
-    public Vector3 TopLeftCorner { get; private set; }
-    public Vector3 TopRightCorner { get; private set; }
+    private Vector3 bottomLeftCorner;
+    private Vector3 bottomRightCorner;
+    private Vector3 topLeftCorner;
+    private Vector3 topRightCorner;
+
+    private GameObject ball;
+
+    public static Vector3 BottomLeftCorner => instance.bottomLeftCorner;
+    public static Vector3 BottomRightCorner => instance.bottomRightCorner;
+    public static Vector3 TopLeftCorner => instance.topLeftCorner;
+    public static Vector3 TopRightCorner => instance.topRightCorner;
+    public static Team Team1 => instance.team1;
+    public static Team Team2 => instance.team2;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
+        bottomLeftCorner = new Vector3(width / 2, 0, height / 2) - transform.position;
+        bottomRightCorner = new Vector3(width / 2, 0, -height / 2) + transform.position;
+        topLeftCorner = new Vector3(-width / 2, 0, height / 2) + transform.position;
+        topRightCorner = new Vector3(width / 2, 0, height / 2) + transform.position;
+
         GameManager.BreedMePlease(team1, team2);
+    }
+
+    public static void Init(GameObject ball)
+    {
+        instance.ball = ball;
     }
 }
