52cb144b58ac53674700c4ad7c7f3bf94c734524
Bryan Engel <bryan.engel@isen.yncrea.fr>
2021-12-16 16:34:33 +0100

    Positioning Players

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -9,6 +9,17 @@ public class Field : MonoBehaviour
 
     [SerializeField] private Team team1, team2;
 
+    [SerializeField] private Vector2 attackPosCaptain;
+    [SerializeField] private Vector2 attackPosMate1;
+    [SerializeField] private Vector2 attackPosMate2;
+    [SerializeField] private Vector2 attackPosMate3;
+
+    [SerializeField] private Vector2 defPosCaptain;
+    [SerializeField] private Vector2 defPosMate1;
+    [SerializeField] private Vector2 defPosMate2;
+    [SerializeField] private Vector2 defPosMate3;
+
+
     public static Team Team1 => instance.team1;
     public static Team Team2 => instance.team2;
 
@@ -24,7 +35,7 @@ public class Field : MonoBehaviour
     private Vector3 topRightCorner;
     public static Vector3 TopRightCorner => instance.topRightCorner;
 
-    // TODO Bryan : Récupérer positions initiales
+    // TODO Bryan : Rï¿½cupï¿½rer positions initiales
 
     private Ball ball;
     public static Ball Ball => instance.ball;
@@ -53,8 +64,33 @@ public class Field : MonoBehaviour
 
         ball.transform.position = instance.transform.position;
 
-        Team1.Players[0].transform.position = ball.transform.position;
+        instance.SetTeamPosition();
 
         // TODO Bryan : Positionner les joueurs
     }
+
+
+    private void SetTeamPosition()
+    {
+        Team1.Players[0].transform.position = VectorToPosition(attackPosCaptain);
+        Team1.Players[1].transform.position = VectorToPosition(attackPosMate1);
+        Team1.Players[2].transform.position = VectorToPosition(attackPosMate2);
+        Team1.Players[3].transform.position = VectorToPosition(attackPosMate3);
+
+        Team2.Players[0].transform.position = VectorToPosition(AxialSymmetry(defPosCaptain));
+        Team2.Players[1].transform.position = VectorToPosition(AxialSymmetry(defPosMate1));
+        Team2.Players[2].transform.position = VectorToPosition(AxialSymmetry(defPosMate2));
+        Team2.Players[3].transform.position = VectorToPosition(AxialSymmetry(defPosMate3));
+    }
+    private Vector3 AxialSymmetry(Vector3 vector)
+    {
+        return transform.position - vector;
+    }
+
+    private Vector3 VectorToPosition(Vector3 vector)
+    {
+        return new Vector3(vector.x * height / 2, vector.z, vector.y * width / 2);
+    }
+
+        
 }
