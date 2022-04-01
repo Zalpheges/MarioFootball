836399554ee305c662da5b0cc21c591ca579b69c
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2022-04-01 23:01:01 +0200

    public anim

--- Assets/Scripts/Player/Spectators/SpectatorBehavior.cs
+++ Assets/Scripts/Player/Spectators/SpectatorBehavior.cs
@@ -6,18 +6,20 @@ public class SpectatorBehavior : MonoBehaviour
 {
     public float ProbaCelebrationWithGoal =0.8f;
     public float ProbaCelebrationWithOUTGoal = 0.1f;
+    public bool isSit;
 
     public SpectatorManager SpectatorManager;
     void Start()
     {
         SpectatorManager = gameObject.transform.parent.GetComponent<SpectatorManager>();
+        gameObject.GetComponent<Animator>().SetBool("isSit", isSit);
     }
 
     void Update()
     {
         float random = Random.Range(0.0f, 1.0f);
-        //Debug.Log(gameObject.name+" " +random);
-        if (SpectatorManager.isGoal && random<= ProbaCelebrationWithGoal/200)
+        
+        if (SpectatorManager.isGoal && random <= ProbaCelebrationWithGoal / 200)
         {
             gameObject.GetComponent<Animator>().SetBool("SitVictory", true);
         }
@@ -25,5 +27,8 @@ public class SpectatorBehavior : MonoBehaviour
         {
             gameObject.GetComponent<Animator>().SetBool("SitVictory", false);
         }
+        
+       
+        
     }
 }
--- Assets/Scripts/Player/Spectators/SpectatorManager.cs
+++ Assets/Scripts/Player/Spectators/SpectatorManager.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class SpectatorManager : MonoBehaviour
 {
-    public GameObject[] Spectators;
     public bool isGoal = false;
-    public int NombreCelebrationWithGoal = 2;
-    public int NombreCelebrationWithOUTGoal = 1;
+    
 
 
     void Start()
@@ -17,21 +15,6 @@ public class SpectatorManager : MonoBehaviour
 
     void Update()
     {
-        /*
-        if (isGoal)
-        {
-            for (int i = 0; i < Spectators.Length; i++)
-            {
-                Spectators[i].GetComponent<Animator>().SetBool("SitVictory", true);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < Spectators.Length; i++)
-            {
-                Spectators[i].GetComponent<Animator>().SetBool("SitVictory", false);
-            }
-        }
-        */
+        
     }
 }
